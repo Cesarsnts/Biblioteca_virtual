dd7d9174793733148b3ee21e1f63c3cd3c8f7154
pudimedo <edopudim@gmail.com>
2026-02-03 14:54:45 -0300

    fredo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
+import './style.css';
 import App from './App'; // VOLTA PARA App NORMAL
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
